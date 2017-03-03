f64ef6009432dd5bbc509db629861a993708dbd9
Vladimir Orany <vladimir@orany.cz>
2017-03-03 13:29:15 +0100

    test for third task - merging and formatting

---
+++ eig.tasks/src/test/groovy/eig/tasks/_03_ExportAsWellFormattedExcelSpec.groovy
@@ -0,0 +1,66 @@
+package eig.tasks
+
+import eig.model.test.TestData
+import eig.model.test.TestFiles
+import org.modelcatalogue.spreadsheet.api.Cell
+import org.modelcatalogue.spreadsheet.query.api.SpreadsheetCriteria
+import org.modelcatalogue.spreadsheet.query.poi.PoiSpreadsheetCriteria
+import spock.lang.Specification
+
+class _03_ExportAsWellFormattedExcelSpec extends Specification {
+
+    void 'export well formatted excel'() {
+        when:
+            File excelFile = TestFiles.newTestFile('test03.xlsx')
+            ExcelExporter.buildWellFormattedSpreadsheet(TestData.orders).writeTo(excelFile)
+            TestFiles.open excelFile
+        and:
+            SpreadsheetCriteria criteria = PoiSpreadsheetCriteria.FACTORY.forFile(excelFile)
+        then: 'there is no exception thrown'
+            noExceptionThrown()
+        and: 'all the prices are displayed starting with EUR sign'
+            criteria.query {
+                sheet {
+                    row {
+                        cell {
+                            style {
+                                format {
+                                    it.startsWith('€')
+            }   }   }   }   }   }.size() == 8469
+        and: 'dates are well-formatted and displayed in merged cells'
+            criteria.query {
+                sheet {
+                    row {
+                        cell {
+                            style {
+                                format {
+                                    it.startsWith('d')
+            }   }   }   }   }   }.size() == 307
+        and: 'resolved rows are displayed in light green'
+            criteria.query {
+                sheet {
+                    row {
+                        cell {
+                            style {
+                                foreground lightGreen
+                            }
+                        }
+                    }
+                }
+            }.size() == 432
+        and: 'headers are bold'
+            criteria.query {
+                sheet {
+                    row {
+                        cell {
+                            style {
+                                font {
+                                    make bold
+                            }   }
+                        }
+                    }
+                }
+            }.size() == 22
+    }
+
+}
